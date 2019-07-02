68dde8b236d6ae9c8413940c280f8b6df6676b06
Miguel <shota@miurashoutanoMacBook-Pro.local>
2019-07-03 06:08:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -95,15 +95,11 @@ $(function() {
     const videoSource = $('#videoSource').val();
     const constraints = {
       audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
-      video: {deviceId: videoSource ? {exact: videoSource} : undefined},
+      video: {deviceId: videoSource ? {exact: videoSource} : undefined,
+      facingMode: { exact: 'environment' }
+      },
     };
 
-    if (window.stream) {
-            window.stream.getTracks().forEach(function(track) {
-                track.stop();
-            });
-        }
-        
     navigator.mediaDevices.getUserMedia(constraints).then(stream => {
       $('#my-video').get(0).srcObject = stream;
       localStream = stream;
